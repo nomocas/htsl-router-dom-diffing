ba595c46fc8021e59931d5746dc1830ad5f718c3
Gilles Coomans <gilles.coomans@gmail.com>
2017-07-10 01:09:31 +0200

    fix(structure): rename engine to index.js

--- src/index.js
+++ src/index.js
@@ -2,42 +2,62 @@
  * @Author: Gilles Coomans
  */
 
-import pathToRegexp from 'path-to-regexp';
-import htmlLexicon from 'htsl-lexicon';
-htmlLexicon.addAtoms(['router']);
-// example : h.router(location, routes, ?props);
+import difPragmas from 'htsl-dom-diffing-pragmatics';
+import router from 'htsl-router-core';
 
-function compil(routes) {
-	const arr = [];
-	for (const path in routes) {
-		const keys = [];
-		arr.push({ keys, path, render: routes[path], pattern: pathToRegexp(path, keys) });
+difPragmas.renderActions.router = function($tag, lexem /* args : location, router, props */ , component, frag) {
+	const matched = lexem.matched = router.matchRoute(lexem.args[0], lexem.args[1]);
+	if (matched) {
+		lexem.developed = matched.route.render(matched.params, lexem.args[2]);
+		difPragmas.render($tag, lexem.developed, component, frag);
 	}
-	return arr;
-}
-
-function matchURI(route, uri) {
-	const match = route.pattern.exec(uri);
-	if (!match) return null;
-	const params = Object.create(null);
-	for (let i = 1; i < match.length; i++)
-		params[route.keys[i - 1].name] = match[i] !== undefined ? match[i] : undefined;
-	return params;
-}
-
-function matchRoute(location, routes) {
-	let found = null;
-	routes.some((route) => {
-		const uri = location.error ? '/error' : location.pathname;
-		const params = matchURI(route, uri);
-		return params && (found = { route, params });
-	});
-	return found;
-}
-
-export default {
-	compil,
-	matchURI,
-	matchRoute
+	lexem.witness = document.createComment('router');
+	$tag.appendChild(lexem.witness);
 };
 
+difPragmas.difActions.router = function($tag, lexem /* args : location, router, props */ , olexem, component) {
+	const matched = lexem.matched = router.matchRoute(lexem.args[0], lexem.args[1]);
+	lexem.witness = olexem.witness;
+	if (matched) {
+		lexem.developed = matched.route.render(matched.params, lexem.args[2]);
+		if (!olexem.matched || olexem.matched.route.path !== matched.route.path) {
+			if (olexem.developed)
+				difPragmas.remove($tag, olexem.developed, component);
+			const frag = document.createDocumentFragment();
+			difPragmas.render($tag, lexem.developed, component, frag);
+			$tag.insertBefore(frag, lexem.witness);
+		} else
+			difPragmas.dif($tag, lexem.developed, olexem.developed, component);
+	} else if (olexem.developed)
+		difPragmas.remove($tag, olexem.developed, component);
+};
+
+difPragmas.removeActions.router = function($tag, lexem /* args : location, router, props  */ , component) {
+	if (lexem.developed)
+		difPragmas.remove($tag, lexem.developed, component);
+};
+
+export default {
+	difPragmas,
+	router
+};
+
+/**
+ ***************************** String Pragmas
+ */
+
+// stringPragmas.pragmas.router = function(descriptor, args /* location, router, props  */ , component) {
+// 	const matched = router.matchRoute(args[0], args[1]);
+// 	if (matched)
+// 		stringPragmas.$output(descriptor, matched.route.render(matched.params), component);
+// };
+
+/**
+ ***************************** Dom Pragmas
+ */
+
+// domPragmas.pragmas.router = function($tag, args /* location, router, props  */ , component) {
+// 	const matched = router.matchRoute(args[0], args[1]);
+// 	if (matched)
+// 		domPragmas.$output($tag, matched.route.render(matched.params), component);
+// };
